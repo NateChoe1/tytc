a69e68382a66a2c2e1aa7c66d8ae855f0978b293
NateChoe1 <nate@nates-laptop.localdomain>
2021-02-21 08:43:27 -0600

    Added some UI and support for related videos on videos

--- scraper.sh
+++ scraper.sh
@@ -18,19 +18,76 @@ getData() {
 	HTML=$(curl -s -A "$USER_AGENT" "$1")
 	ID="No IDs found"
 	TITLE="No titles found"
-	if (echo "$1" | grep -P "^https://www.youtube.com/?$");
+	if [[ "$1" =~ ^https://www.youtube.com/?$ ]]
 	then
-		DATA_RAW=$(echo "$HTML" | grep -Po "{\"videoId\":\"[a-zA-Z0-9\-_]{11}\",.*?\"text\":\".*?[^\\\\]\"")
-		ID=$(echo "$DATA_RAW" | cut -b 13-23)
+		DATA_RAW=$(echo "$HTML" | grep -Po "{\"videoId\":\"[a-zA-Z0-9_-]{11}\",.*?\"text\":\".*?[^\\\\]\"" | cut -b 13- | uniq -w 11)
+		#includes the name of the video and the id
+		ID=$(echo "$DATA_RAW" | cut -b 1-11)
 		TITLE=$(echo "$DATA_RAW" | grep -Po "((?<![\\\\])\")((?:.(?!(?<![\\\\])\1))*.?)\1$" | cut -b 2- | sed 's/.$//')
 		#https://www.metaltoad.com/blog/regex-quoted-string-escapable-quotes
-	elif (echo "$1" | grep -P "^https://www.youtube.com/watch\?v=[a-zA-Z0-9\-_]{11}/?$");
+	elif [[ "$1" =~ ^https://www.youtube.com/watch\?v=[a-zA-Z0-9_-]{11}/?$ ]]
 	then
-		ID=$(echo "$HTML" | grep -Po "\"videoId\":\"[a-zA-Z0-9\-_]{11}\"" | cut -b 12-22 | uniq | awk "NR > 1")
-		TITLE=$(echo "$HTML" | grep -Po ",\"simpleText\":\".*?\"\}" | tail -n +5 | cut -b 16- | sed "s/..$//" | awk "NR % 3 == 1")
+		DATA_RAW=$(echo "$HTML" | grep -Po "\"simpleText\":((?<![\\\\])\")((?:.(?!(?<![\\\\])\1))*.?)\1},\"shortBylineText\":.*?\"videoId\":\"[a-zA-Z0-9_-]{11}\"")
+		#includes the name of the video and the id
+		ID=$(echo "$DATA_RAW" | grep -Po "\"videoId\":\"[a-zA-Z0-9_-]{11}\"" | cut -b 12-22)
+		TITLE=$(echo "$DATA_RAW" | grep -Po "^\"simpleText\":((?<![\\\\])\")((?:.(?!(?<![\\\\])\1))*.?)\1" | cut -b 15- | sed 's/.$//');
+	else
+		echo "Didn't match"
 	fi
+	#This could probably be done with a case statement but the second regex would be massive.
+
 	paste <(echo "$ID") <(echo "$TITLE")
 }
+#getData takes in a youtube link as an input and outputs all the related videos (or front page videos in the case of https://www.youtube.com/) in this form:
+#[11 characters for the id of the video]	[the title of the video]
+#[11 characters for the next video id  ]	[the title of the next video]
+#...
 
-#getData "https://www.youtube.com"
-getData "https://www.youtube.com/watch?v=KcZn05qxVgg"
+viewVideo() {
+	vlc "$1"
+}
+
+URL="N/A"
+VIDEOS="N/A"
+while :
+do
+	echo "Enter in the next action (? for help)"
+	read ACTION
+
+	case "$ACTION" in
+		\?)
+			echo "Commands:"
+			echo "?     Show this screen"
+			echo "h     Go to the youtube homepage"
+			echo "u     Select a video to view based on url"
+			echo "/     Search on youtube"
+			echo "d     Display the current URL and suggested videos"
+			echo "p     Play the current video"
+			echo "q     Quit the program"
+			;;
+		h)
+			URL="https://www.youtube.com/"
+			VIDEOS=$(getData "$URL")
+			;;
+		u)
+			echo "Enter in the youtube video to go to"
+			read URL
+			VIDEOS=$(getData "$URL")
+			;;
+
+		d)
+			echo "You are watching $URL"
+			echo "Suggested videos:"
+			echo "$VIDEOS" | awk '{printf("%d	%s\n", NR, $0)}'
+			;;
+		p)
+			viewVideo "$URL"
+			;;
+		q)
+			break
+			;;
+		*)
+			echo "Invalid command."
+			;;
+	esac
+done

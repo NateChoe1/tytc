80de955d07876148b20acbde45dd4b4b7c936b53
NateChoe1 <nate@nates-laptop.localdomain>
2021-03-04 23:22:23 -0600

    Added a way to go to the channel of a video

--- scraper.sh
+++ scraper.sh
@@ -14,15 +14,19 @@
 #!/bin/bash
 USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0"
 
+ID_LENGTH=11
+#Youtube ids are 11 characters that are capital and lowercase letters, letters, hyphens and underscores.
+
+HTML=""
+
 getData() {
-	HTML=$(curl -s -A "$USER_AGENT" "$1")
 	ID="No IDs found"
 	TITLE="No titles found"
 	STRING_REGEX="((?<![\\\\])\")((?:.(?!(?<![\\\\])\1))*.?)\""
 	#https://www.metaltoad.com/blog/regex-quoted-string-escapable-quotes
 	if [[ "$1" =~ ^https://www.youtube.com/?$ ]]
 	then
-		DATA_RAW=$(echo "$HTML" | grep -Po "{\"videoId\":\"[a-zA-Z0-9_-]{11}\",.*?\"text\":\".*?[^\\\\]\"" | cut -b 13- | uniq -w 11)
+		DATA_RAW=$(echo "$HTML" | grep -Po "{\"videoId\":$STRING_REGEX,.*?\"text\":$STRING_REGEX" | cut -b 13- | uniq -w 11)
 		#includes the name of the video and the id
 		ID=$(echo "$DATA_RAW" | cut -b 1-11)
 		TITLE=$(echo "$DATA_RAW" | grep -Po "$STRING_REGEX$" | cut -b 2- | sed 's/.$//')
@@ -34,7 +38,7 @@ getData() {
 		TITLE=$(echo "$DATA_RAW" | grep -Po "^\"simpleText\":$STRING_REGEX" | cut -b 15- | sed 's/.$//');
 	elif [[ $1 =~ ^https://www.youtube.com/results\?search_query=.*/?$ ]]
 	then
-		DATA_RAW=$(echo "$HTML" | grep -Po "\"videoId\":\"[a-zA-Z0-9_-]{11}\",\"thumbnail\".*?\"text\":$STRING_REGEX" | cut -b 12-)
+		DATA_RAW=$(echo "$HTML" | grep -Po "\"videoId\":$STRING_REGEX,\"thumbnail\".*?\"text\":$STRING_REGEX" | cut -b 12-)
 		ID=$(echo "$DATA_RAW" | cut -b 1-11)
 		TITLE=$(echo "$DATA_RAW" | grep -Po "$STRING_REGEX$" | cut -b 2- | sed 's/.$//')
 	fi
@@ -47,15 +51,28 @@ getData() {
 #[11 characters for the next video id  ]	[the title of the next video]
 #...
 
+getChannelCreator() {
+	STRING_REGEX="((?<![\\\\])\")((?:.(?!(?<![\\\\])\1))*.?)\""
+	#TODO: Cache the html
+	if [[ "$1" =~ ^https://www.youtube.com/watch\?v=[a-zA-Z0-9_-]{11}/?$ ]]
+	then
+		DATA_RAW=$(echo "$HTML" | grep -Po "\"channelId\":$STRING_REGEX,\"isOwnerViewing\":.*?\"author\":$STRING_REGEX")
+		#includes the name of the video and the id
+		CHANNEL_NAME=$(echo "$DATA_RAW" | grep -Po "^\"channelId\":$STRING_REGEX" | cut -b 14- | sed "s/.$//")
+		CHANNEL_ID=$(echo "$DATA_RAW" | grep -Po "\"author\":$STRING_REGEX$" | cut -b 11- | sed "s/.$//")
+		echo "$CHANNEL_NAME$CHANNEL_ID"
+	fi
+}
+
 viewVideo() {
-	vlc "$1"
+	mpv "$1"
 }
 
 viewThumbnail() {
 	feh "https://i.ytimg.com/vi/$1/maxresdefault.jpg" || feh "https://i.ytimg.com/vi/$1/mqdefault.jpg" || feh "https://i.ytimg.com/vi/$1/default.jpg"
 }
 
-#Everything above this is where the scraping occurs. As long as these functions work, the entire thing will work. The higher up the function is the more at risk the function is of breaking.
+#Everything above this is where the scraping occurs. As long as these functions work, the entire thing will work. The higher up the function is the more at risk the function is of breaking. To port this to another site, just make sure these functions work.
 
 displayVideos() {
 	echo "$1" | nl -n ln -b a
@@ -84,23 +101,28 @@ do
 			echo "p     Play the current video"
 			echo "vc    View the thumbnail of the current video"
 			echo "vs    View the thumbnail of a suggested video"
+			echo "c     See information on the channel of the video"
+			echo "cc    Go to the channel of the video (IN BETA)"
 			echo "q     Quit the program"
 			;;
 		h)
 			URL="https://www.youtube.com/"
+			HTML=$(curl -s -A "$USER_AGENT" "$URL")
 			VIDEOS=$(getData "$URL")
 			;;
 		u)
 			echo "Enter in the youtube video to go to"
 			read URL
+			HTML=$(curl -s -A "$USER_AGENT" "$URL")
 			VIDEOS=$(getData "$URL")
 			;;
 		s)
 			displayVideos "$VIDEOS"
 			echo "Enter in the video to go to"
 			read LINE
-			ID=$(echo "$VIDEOS" | cut -b 1-11 | sed "${LINE}q;d")
+			ID=$(echo "$VIDEOS" | cut -b 1-$ID_LENGTH | sed "${LINE}q;d")
 			URL=$(echo "https://www.youtube.com/watch?v=$ID")
+			HTML=$(curl -s -A "$USER_AGENT" "$URL")
 			VIDEOS=$(getData "$URL")
 			;;
 		/)
@@ -108,6 +130,7 @@ do
 			read QUERY
 			QUERY=$(sanitizeSearch "$QUERY")
 			URL="https://www.youtube.com/results?search_query=$QUERY"
+			HTML=$(curl -s -A "$USER_AGENT" "$URL")
 			VIDEOS=$(getData "$URL")
 			;;
 		d)
@@ -125,7 +148,19 @@ do
 			displayVideos "$VIDEOS"
 			echo "Enter in the video to view the thumbnail of"
 			read LINE
-			viewThumbnail "$(echo "$VIDEOS" | cut -b 1-11 | sed "${LINE}q;d")"
+			viewThumbnail "$(echo "$VIDEOS" | cut -b 1-$ID_LENGTH | sed "${LINE}q;d")"
+			;;
+		c)
+			CHANNEL_DATA=$(getChannelCreator "$URL")
+			CHANNEL_ID=$(echo "$CHANNEL_DATA" | cut -b 1-24)
+			CHANNEL_NAME=$(echo "$CHANNEL_DATA" | cut -b 25-)
+			echo "Channel id: $CHANNEL_ID"
+			echo "Channel name: $CHANNEL_NAME"
+			;;
+		cc)
+			CHANNEL_DATA=$(getChannelCreator "$URL")
+			CHANNEL_ID=$(echo "$CHANNEL_DATA" | cut -b 1-24)
+			URL="https://www.youtube.com/channel/${CHANNEL_ID}/videos"
 			;;
 		q)
 			break
